5e1743f5af93eac9e9dc0a6286b5364378d04456
honght97 <hong.ht150297@gmail.com>
2024-04-14 15:57:04 +0700

    setState

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,10 +8,26 @@ class MyComponent extends React.Component {
     age: 27,
     address: "HCM",
   };
+  handleClick = (e) => {
+    // console.log(this);
+    // console.log(">>> click me my button");
+    console.log("My name is  ", this.state.name);
+    this.setState({
+      name: "hong123",
+      age: 26,
+    });
+  };
   render() {
     return (
       <div>
         my name is {this.state.name} and i'm from {this.state.address}
+        <button
+          onClick={(e) => {
+            this.handleClick(e);
+          }}
+        >
+          click me
+        </button>
       </div>
     );
   }
