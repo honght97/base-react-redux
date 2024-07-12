fcebf5d61a36f115b1d2e98cdfa7f3a63fc50425
honght97 <hong.ht150297@gmail.com>
2024-07-12 15:52:18 +0700

    props and conditional

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,15 +1,43 @@
 import React from "react";
 class DisplayInfo extends React.Component {
+  state = {
+    isShow: true,
+  };
+  handleToggleListUsers() {
+    this.setState({
+      isShow: !this.state.isShow,
+    });
+  }
   render() {
     // porps => viet tat properties(thuoc tinh / tai san)
     // desctructuring array/object
-    const { name, age } = this.props;
-    console.log(this.props);
+    const { listUsers } = this.props;
+    // console.log(this.props);
+
     return (
-      <div>
-        <p>My name's {name}</p>
-        <p>My age's {age}</p>
-      </div>
+      <>
+        <div>
+          <button
+            onClick={() => {
+              this.handleToggleListUsers();
+            }}
+          >
+            {this.state.isShow ? "Hide " : "Show "}
+            list user
+          </button>
+        </div>
+        {this.state.isShow && (
+          <ul>
+            {listUsers.map((user) => {
+              return (
+                <li key={user.id} className={+user.age > 28 ? "red" : "green"}>
+                  My name's {user.name} and i'm {user.age} years old
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </>
     );
   }
 }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,13 +4,21 @@ import React from "react";
 import UserInfo from "./UserInfo";
 import DisplayInfo from "./DisplayInfo";
 class MyComponent extends React.Component {
+  state = {
+    listUsers: [
+      { id: 1, name: "Hoi Dan IT", age: 30 },
+      { id: 2, name: "Eric", age: 26 },
+      { id: 3, name: "HarryPhamDev", age: 28 },
+      { id: 4, name: "BK", age: 35 },
+    ],
+  };
   // jsx
   render() {
     return (
       <div>
         <UserInfo />
         <br /> <br />
-        <DisplayInfo name="Hoi Dan IT" age="30" />
+        <DisplayInfo listUsers={this.state.listUsers} />
       </div>
     );
   }
