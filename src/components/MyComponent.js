43b69e6de56e0525162a4130d4ecca4f9e08c316
honght97 <hong.ht150297@gmail.com>
2024-07-12 09:36:27 +0700

    react component and class component

---
+++ src/components/MyComponent.js
@@ -0,0 +1,10 @@
+// class component
+// function component
+import React from "react";
+class MyComponent extends React.Component {
+  // jsx
+  render() {
+    return <div>My first component</div>;
+  }
+}
+export default MyComponent;
