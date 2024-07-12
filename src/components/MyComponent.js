bc022d7d58eccee44c09381a1c96417e8a3713fb
honght97 <hong.ht150297@gmail.com>
2024-07-12 11:08:52 +0700

    update state with setSate

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,12 +8,27 @@ class MyComponent extends React.Component {
     address: "Hoi Dan IT",
     age: 26,
   };
+  handleClick(event) {
+    console.log("my name is: ", this.state.name);
+    // merge state => react class
+    this.setState({
+      name: "HarryPhamDev",
+    });
+  }
 
   // jsx
   render() {
     return (
       <div>
         My name is {this.state.name} and i'm from {this.state.address}
+        <br />
+        <button
+          onClick={(event) => {
+            this.handleClick(event);
+          }}
+        >
+          click me
+        </button>
       </div>
     );
   }
