fa1d9fb96576882ce24b8ceed73e46d555c85d89
honght97 <hong.ht150297@gmail.com>
2024-07-12 09:53:43 +0700

    state in react

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,9 +2,20 @@
 // function component
 import React from "react";
 class MyComponent extends React.Component {
+  // state
+  state = {
+    name: "Eric",
+    address: "Hoi Dan IT",
+    age: 26,
+  };
+
   // jsx
   render() {
-    return <div>My first component</div>;
+    return (
+      <div>
+        My name is {this.state.name} and i'm from {this.state.address}
+      </div>
+    );
   }
 }
 export default MyComponent;
